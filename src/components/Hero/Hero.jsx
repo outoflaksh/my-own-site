141462e60dcf1621d9e5b15ff9332934398e5095
laksh <lakshyam38@gmail.com>
2022-08-16 11:23:21 +0530

    Complete initial hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,15 @@
 import "../../styles/layout.css";
+import "../../styles/hero.css";
+import Heading from "./Heading";
+import Terminal from "./Terminal";
 
 const Hero = () => {
-    return <section className="page-section bg1">Hero</section>;
+    return (
+        <section className="page-section bg1 hero-container">
+            <Heading />
+            <Terminal />
+        </section>
+    );
 };
 
 export default Hero;
---
+++ src/components/Hero/Terminal.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Terminal = () => {
+    const links = [
+        "github.com/outoflaksh",
+        "linkedin.com/in/lakshya-malik",
+        "linktr.ee/lakshya",
+    ];
+
+    return (
+        <div className="terminal bg2">
+            <p className="cmd">
+                <span>~$</span> ./neofetch
+            </p>
+            <div className="output">
+                <img src="./logo-ascii.png" className="ascii-logo" />
+                <div className="stats">
+                    <p>Links:</p>
+                    {links.map((link) => {
+                        return (
+                            <li>
+                                <a href={link} target="_blank" rel="noreferrer">
+                                    {link}
+                                </a>
+                            </li>
+                        );
+                    })}
+                    <br />
+                    <p>Stats:</p>
+                    <li>Total commits: 783</li>
+                    <li>Stars earned: 36</li>
+                    <li>Total Leetcode solved: 92</li>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Terminal;
