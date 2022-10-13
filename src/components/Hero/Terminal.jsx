b0ad8174983dde25e554c21ab7aeaf3aa681a0f0
laksh <lakshyam38@gmail.com>
2022-10-13 23:13:52 +0530

    added gradient background to the terminal

--- src/components/Hero/Terminal.jsx
+++ src/components/Hero/Terminal.jsx
@@ -8,28 +8,35 @@ const Terminal = () => {
     ];
 
     return (
-        <div className="terminal bg2">
-            <p className="cmd">
-                <span>~$</span> ./neofetch
-            </p>
-            <div className="output">
-                <img src="./logo-ascii.png" className="ascii-logo" />
-                <div className="stats">
-                    <p>Links:</p>
-                    {links.map((link) => {
-                        return (
-                            <li>
-                                <a href={link} target="_blank" rel="noreferrer">
-                                    {link}
-                                </a>
-                            </li>
-                        );
-                    })}
-                    <br />
-                    <p>Stats:</p>
-                    <li>Total commits: 783</li>
-                    <li>Stars earned: 36</li>
-                    <li>Total Leetcode solved: 92</li>
+        <div className="terminal-wrapper">
+            <div className="gradient-bg"></div>
+            <div className="terminal bg2">
+                <p className="cmd">
+                    <span>~$</span> ./neofetch
+                </p>
+                <div className="output">
+                    <img src="./logo-ascii.png" className="ascii-logo" />
+                    <div className="stats">
+                        <p>Links:</p>
+                        {links.map((link) => {
+                            return (
+                                <li>
+                                    <a
+                                        href={link}
+                                        target="_blank"
+                                        rel="noreferrer"
+                                    >
+                                        {link}
+                                    </a>
+                                </li>
+                            );
+                        })}
+                        <br />
+                        <p>Stats:</p>
+                        <li>Total commits: 783</li>
+                        <li>Stars earned: 36</li>
+                        <li>Total Leetcode solved: 92</li>
+                    </div>
                 </div>
             </div>
         </div>
